c3081476db0999c1876e65eaf7b9a836240459bc
Adam Ahmed <avahmed@ualberta.ca>
2023-02-08 21:48:21 -0700

    init health app

--- api/settings.py
+++ api/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'rest_framework'
+    'rest_framework',
+    'health'
 ]
 
 MIDDLEWARE = [

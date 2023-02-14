81bc20a467fc9f449c5eb3ab3abd841abddba50c
Adam Ahmed <avahmed@ualberta.ca>
2023-02-13 22:33:28 -0700

    include date and filename

--- api/utils/model_abstracts.py
+++ api/utils/model_abstracts.py
@@ -1,8 +1,9 @@
-# 
+# 2023-02-13
+# utils/model_abstracts.py
+
 # This code came from a video tutorial from Bobby Stearman on 2022-11-28 retrieved on 2023-02-12, to Youtube freeCodeCamp.org
 # video here:
 # https://www.youtube.com/watch?v=tujhGdn1EMI
-# 
 
 import uuid
 from django.db import models

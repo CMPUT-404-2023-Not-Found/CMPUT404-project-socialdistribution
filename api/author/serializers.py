eca0bf7d6c95c7a7e9eb8cca1585e340e0cf5fe7
Adam Ahmed <avahmed@ualberta.ca>
2023-02-13 22:33:45 -0700

    enforce username, password, and host requirement

--- api/author/serializers.py
+++ api/author/serializers.py
@@ -2,15 +2,15 @@
 # author/serializers.py
 
 from rest_framework import serializers
-from rest_framework.fields import CharField, DateTimeField, IntegerField, URLField, UUIDField
+from rest_framework.fields import CharField, URLField
 from .models import Author
 
 class AuthorSerializer(serializers.ModelSerializer):
+    username    = CharField(required=True)
+    password    = CharField(required=True)
+    host        = URLField(required=True)
 
     class Meta:
         model = Author
-        fields = (  'id', 'username', 
-                    'createdOn', 'lastModified', 
+        fields = (  'username', 'password',
                     'displayName', 'github', 'host', 'profileImage')
-                
-        

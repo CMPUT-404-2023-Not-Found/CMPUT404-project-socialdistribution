01b6e6c49cad3e37a2bbf428d50c54640285fee9
Adam Ahmed <avahmed@ualberta.ca>
2023-02-14 11:26:21 -0700

    change double quote to single to be consistent

--- api/author/models.py
+++ api/author/models.py
@@ -11,19 +11,19 @@ class Author(Model):
     password        = models.CharField(max_length=64)
 
     # Modification fields
-    createdOn       = models.DateTimeField(auto_now=False, auto_now_add=True, verbose_name="Creation On")
-    lastModified    = models.DateTimeField(auto_now=True, auto_now_add=False, verbose_name="Last Modified On")
+    createdOn       = models.DateTimeField(auto_now=False, auto_now_add=True, verbose_name='Creation On')
+    lastModified    = models.DateTimeField(auto_now=True, auto_now_add=False, verbose_name='Last Modified On')
     rev             = models.IntegerField(default=0)
     
     # Personalization fields
-    displayName     = models.CharField(default="", max_length=128, verbose_name="Display Name")
-    github          = models.URLField(default="", max_length=128, verbose_name="GitHub")
+    displayName     = models.CharField(default='', max_length=128, verbose_name='Display Name')
+    github          = models.URLField(default='', max_length=128, verbose_name='GitHub')
     host            = models.URLField(max_length=128)
-    profileImage    = models.URLField(default="", max_length=128, verbose_name="Profile Image")
+    profileImage    = models.URLField(default='', max_length=128, verbose_name='Profile Image')
 
     class Meta:
-        verbose_name = "Author"
-        verbose_name_plural = "Authors"
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authors'
 
     def __str__(self):
         return f'{self.username}'

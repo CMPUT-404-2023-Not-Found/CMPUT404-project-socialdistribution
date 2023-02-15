aa2fc6f6c4d57043fc06f950694a1d5f2ec7614f
Adam Ahmed <avahmed@ualberta.ca>
2023-02-15 01:14:42 -0700

    add GUI form in admin for creating and updating authors

--- api/author/admin.py
+++ api/author/admin.py
@@ -2,7 +2,34 @@
 # author/admin.py
 
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
+from .forms import AuthorCreationForm, AuthorChangeForm
 from .models import Author
+# This code is modified from an article from Michael Herman on 2023-01-22 retrieved on 2023-02-15, to testdriven.io
+# article here:
+# https://testdriven.io/blog/django-custom-user-model/#forms
+class AuthorAdmin(UserAdmin):
+    add_form = AuthorCreationForm
+    form = AuthorChangeForm
+    model = Author
+    list_display = ('id', 'username', 'host', 'display_name', 'github', 'profile_image', 'is_staff', 'is_active')
+    list_filter = ('id', 'username', 'host', 'display_name', 'github', 'profile_image', 'is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('username', 'password', 'host')}),
+        ('Permissions', {'fields': ("is_staff", "is_active", "groups", "user_permissions")})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'id', 'username', 'host',
+                'password1', 'password2', 
+                'is_staff', 'is_active', 'groups', 'user_permissions'
+            )}
+        ),
+    )
+    search_fields = ('id', 'username', 'host')
+    ordering = ('id', 'username', 'host')
 
-admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)

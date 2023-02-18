ec9b415d5548bc8b8c174571e647ad19d3d1e2e5
Adam Ahmed <avahmed@ualberta.ca>
2023-02-18 15:03:08 -0700

    init GET, POST, and DELETE view for inbox

---
+++ api/inbox/serializer.py
@@ -0,0 +1,23 @@
+# 2023-02-18
+# inbox/serialzer.py
+
+from rest_framework import serializers
+from rest_framework.fields import DateTimeField, UUIDField, URLField
+
+from .models import Inbox
+
+class InboxSerializer(serializers.ModelSerializer):
+    id              = UUIDField(read_only=True)
+    author_id       = UUIDField(read_only=True)
+    receivedAt      = DateTimeField(source='received_at', read_only=True)
+    object          = URLField(source='object_id', required=True)
+    senderAuthorId  = URLField(source='sender_author_id', required=True)
+
+    class Meta:
+        model = Inbox
+        fields = [  'id',
+                    'author_id',
+                    'receivedAt',
+                    'object', 'senderAuthorId',
+                    'context', 'summary', 'type',
+        ]

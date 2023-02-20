588d5a31ea7ce15fe9b27a8f18909c06f1bed3b4
Gurveer Sohal <gurveersohal433@gmail.com>
2023-02-20 16:36:32 -0700

    making the form

--- ui/src/pages/CreatePost.js
+++ ui/src/pages/CreatePost.js
@@ -10,7 +10,16 @@ const CreatePost = () => {
     const [description, setDescription] = useState('');
     const [contentType, setContentType] = useState('');
     const [visibility, setVisibility] = useState('');
-    const [unlisted, setUnlisted] = useState('');
+    const [unlisted, setUnlisted] = useState(false);
+
+    const createPost = () => {
+        console.log(title);
+        console.log(content);
+        console.log(description);
+        console.log(contentType);
+        console.log(visibility);
+        console.log(unlisted);
+    }
 
     return (
         <>
@@ -22,11 +31,11 @@ const CreatePost = () => {
 
             <form action="" method="post">
                 <label for="title">Title:</label>
-                <input type="text" id="title" name="post_title" />
+                <input type="text" id="title" name="post_title" onChange={(e) => setTitle(e.target.value)}/>
                 <br></br>
 
                 <label for="description">Description:</label>
-                <input type="text" id="description" name="post_description" />
+                <input type="text" id="description" name="post_description" onChange={(e) => setDescription(e.target.value)} />
                 <br></br>
 
                 <label for="content">Content:</label>
@@ -34,26 +43,26 @@ const CreatePost = () => {
                     {
                         verticalAlign: 'top'
                     }
-                }></textarea>
+                } onChange={(e) => setContent(e.target.value)}></textarea>
                 <br></br>
 
-                <select name="contentType" id="contentType">
+                <select name="contentType" id="contentType" onChange={(e) => setContentType(e.target.value)}>
                     <option value="">--Content Type--</option>
                     <option value="text/plain">Plain text</option>
                 </select>
                 <br></br>
 
-                <select name="visibility" id="visibility">
+                <select name="visibility" id="visibility" onChange={(e) => setVisibility(e.target.visibility)}>
                     <option value="">--Visibility--</option>
                     <option value="public">Public</option>
                 </select>
                 <br></br>
 
-                <input type="checkbox" id="unlisted" name="unlisted" />
+                <input type="checkbox" id="unlisted" name="unlisted" onChange={(e) => setUnlisted(e.target.visibility)}/>
                 <label for="unlisted">Unlisted</label>
                 <br></br>
 
-                <button type="button">Send your message</button>
+                <button type="button" onClick={createPost()}>Send your message</button>
                 <br></br>
             </form>
 

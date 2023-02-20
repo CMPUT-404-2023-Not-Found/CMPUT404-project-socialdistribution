b3dff19f24c35f61f7d630344f52d998f1fe4339
Gurveer Sohal <gurveersohal433@gmail.com>
2023-02-20 16:11:35 -0700

    making the form

--- ui/src/pages/CreatePost.js
+++ ui/src/pages/CreatePost.js
@@ -1,20 +1,62 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const CreatePost = () => {
     let navigate = useNavigate();
 
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const [description, setDescription] = useState('');
+    const [contentType, setContentType] = useState('');
+    const [visibility, setVisibility] = useState('');
+    const [unlisted, setUnlisted] = useState('');
+
     return (
         <>
-            <div>
-                Place holder for post form
-            </div>
-
             <button onClick={() => {
                 navigate('/');
             }}>
                 Go back to stream
             </button>
+
+            <form action="" method="post">
+                <label for="title">Title:</label>
+                <input type="text" id="title" name="post_title" />
+                <br></br>
+
+                <label for="description">Description:</label>
+                <input type="text" id="description" name="post_description" />
+                <br></br>
+
+                <label for="content">Content:</label>
+                <textarea id="content" name="post_content" style={
+                    {
+                        verticalAlign: 'top'
+                    }
+                }></textarea>
+                <br></br>
+
+                <select name="contentType" id="contentType">
+                    <option value="">--Content Type--</option>
+                    <option value="text/plain">Plain text</option>
+                </select>
+                <br></br>
+
+                <select name="visibility" id="visibility">
+                    <option value="">--Visibility--</option>
+                    <option value="public">Public</option>
+                </select>
+                <br></br>
+
+                <input type="checkbox" id="unlisted" name="unlisted" />
+                <label for="unlisted">Unlisted</label>
+                <br></br>
+
+                <button type="button">Send your message</button>
+                <br></br>
+            </form>
+
         </>
     );
 }

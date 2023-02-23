43e9b46d4b00cc6a3cac655e4f17f68b0d8030a5
Adam Ahmed <avahmed@ualberta.ca>
2023-02-22 20:22:05 -0700

    add more TODO create post tests

--- api/post/tests/test_models.py
+++ api/post/tests/test_models.py
@@ -41,6 +41,36 @@ class PostModelTests(Base):
     def test_create_with_blacklist_title(self):
         pass
     
+    def test_create_with_missing_source(self):
+        pass
+
+    def test_create_with_blank_source(self):
+        pass
+
+    def test_create_with_long_source(self):
+        pass
+
+    def test_create_with_non_url_source(self):
+        pass
+
+    def test_create_with_source_of_unknown_node(self):
+        pass
+
+    def test_create_with_missing_origin(self):
+        pass
+
+    def test_create_with_blank_origin(self):
+        pass
+
+    def test_create_with_long_origin(self):
+        pass
+
+    def test_create_with_non_url_origin(self):
+        pass
+
+    def test_create_with_origin_of_unknown_node(self):
+        pass
+    
     def test_create_with_missing_desc(self):
         pass
 
@@ -51,4 +81,82 @@ class PostModelTests(Base):
         pass
 
     def test_create_with_blacklist_desc(self):
+        pass
+
+    def test_create_with_missing_contenttype(self):
+        pass
+
+    def test_create_with_blank_contenttype(self):
+        pass
+
+    def test_create_with_long_contenttype(self):
+        pass
+
+    def test_create_with_unknown_contenttype(self):
+        pass
+
+    def test_create_with_missing_content(self):
+        pass
+
+    def test_create_with_blank_content(self):
+        pass
+
+    def test_create_with_long_content(self):
+        pass
+
+    def test_create_with_blacklist_content(self):
+        pass
+
+    def test_create_with_missing_author(self):
+        pass
+
+    def test_create_with_blank_author(self):
+        pass
+
+    def test_create_with_unknown_author(self):
+        pass
+
+    def test_create_with_mismatched_authors(self):
+        pass
+
+    def test_create_with_non_uuid_author(self):
+        pass
+
+    def test_create_with_missing_categories(self):
+        pass
+
+    def test_create_with_blank_categories(self):
+        pass
+
+    def test_create_with_blacklist_categories(self):
+        pass
+
+    def test_create_with_one_category(self):
+        pass
+
+    def test_create_with_multiple_categories(self):
+        pass
+
+    def test_create_with_missing_visibility(self):
+        pass
+
+    def test_create_with_blank_visibility(self):
+        pass
+
+    def test_create_with_public_visibility(self):
+        pass
+
+    def test_create_with_friends_visibility(self):
+        pass
+
+    def test_create_with_unknown_visibility(self):
+        pass
+    
+    def test_create_with_missing_unlisted(self):
+        pass
+
+    def test_create_with_blank_unlisted(self):
+        pass
+
+    def test_create_with_unknown_unlisted(self):
         pass

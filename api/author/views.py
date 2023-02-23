29bbe1cfdf7e7c14fbd8e8b49ec81956779f4969
Adam Ahmed <avahmed@ualberta.ca>
2023-02-23 11:20:22 -0700

    add to AuthorView ability to retrieve list of authors ordered by username

--- api/author/views.py
+++ api/author/views.py
@@ -1,8 +1,9 @@
 # 2023-02-13
 # author/views.py
 
-from django.shortcuts import render
-from rest_framework import generics, status
+from django.db.models.functions import Lower
+from rest_framework import status
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 import logging
 
@@ -12,8 +13,20 @@ from .serializers import CreateAuthorSerializer
 logger = logging.getLogger('django')
 rev = 'rev: $xGahyt8$x'
 
-class AuthorView(generics.GenericAPIView):
+class AuthorView(ListCreateAPIView):
     serializer_class = CreateAuthorSerializer
+    queryset = Author.objects.all()
+
+    # GET /api/authors/?page=x&size=y
+    def get_queryset(self):
+        logger.info(rev)
+        if (self.request.query_params): # type: ignore
+            logger.info('Getting list of author with query_params [%s]', str(self.request.query_params)) # type: ignore
+        else:
+            logger.info('Getting list of author')
+        return self.queryset.order_by(Lower('username'))
+
+    # POST /api/authors
     def post(self, request):
         logger.info(rev)
         logger.info('Creating new author')

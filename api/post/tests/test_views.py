d6dc1b0b039935c80d8c7b1cf16ecd2056717be6
Adam Ahmed <avahmed@ualberta.ca>
2023-02-22 21:26:45 -0700

    add TODO post view test

--- api/post/tests/test_views.py
+++ api/post/tests/test_views.py
@@ -26,7 +26,7 @@ class PostViewTests(Base):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, []) # type: ignore
     
-    def test_list_author_single_post(self):
+    def test_list_single_post(self):
         '''
         Test one post
         '''
@@ -36,21 +36,174 @@ class PostViewTests(Base):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertCountEqual(response.data, [1]) # type: ignore
 
-    def test_list_author_multiple_posts(self):
+    def test_list_multiple_posts(self):
         '''
         Test multiple posts
         '''
         pass
+    
+    def test_list_too_many_posts(self):
+        pass
+    
+    def test_list_with_unknown_author(self):
+        pass
 
+    def test_list_with_bad_author_uuid(self):
+        pass
+
+    '''
+    Test Post view GET /authors/uuid/posts/?page=x&size=y
+    '''
+    def test_list_with_zero_page(self):
+        pass
+
+    def test_list_with_one_page(self):
+        pass
+
+    def test_list_with_multiple_page(self):
+        pass
+
+    def test_list_with_max_pages(self):
+        pass
+
+    def test_list_with_bad_page_value(self):
+        pass
+
+    def test_list_with_blank_page_value(self):
+        pass
+
+    def test_list_with_zero_page_size(self):
+        pass
+    
+    def test_list_with_one_page_size(self):
+        pass
+    
+    def test_list_with_multiple_page_size(self):
+        pass
+    
+    def test_list_with_max_page_size(self):
+        pass
+
+    def test_list_with_bad_page_size_value(self):
+        pass
+    
+    def test_list_with_reverse_query(self):
+        '''
+        Test with query params in reverse order
+        '''
+        pass
+
+    '''
+    Test Post view GET /authors/uuid/posts/uuid
+    '''
     def test_get_post_detail(self):
         pass
 
+    def test_get_post_without_author_uuid(self):
+        pass
+
+    def test_get_post_with_bad_author_uuid(self):
+        pass
+
+    def test_get_post_with_unknown_author_uuid(self):
+        pass
+
+    def test_get_post_without_post_uuid(self):
+        pass
+
+    def test_get_post_with_bad_post_uuid(self):
+        pass
+
+    def test_get_post_with_unknown_post_uuid(self):
+        pass
+    
+    '''
+    Test Post view POST /authors/uuid/posts/uuid
+    '''
     def test_update_post(self):
         pass
-
-    def test_create_post_with_id(self):
+    
+    def test_update_post_without_author_uuid(self):
         pass
 
+    def test_update_post_with_bad_author_uuid(self):
+        pass
+
+    def test_update_post_with_unknown_author_uuid(self):
+        pass
+    
+    def test_update_post_with_missmatched_author_uuid(self):
+        pass
+
+    def test_update_post_without_post_uuid(self):
+        pass
+
+    def test_update_post_with_bad_post_uuid(self):
+        pass
+
+    def test_update_post_with_unknown_post_uuid(self):
+        pass
+
+    def test_update_post_with_missmatched_post_uuid(self):
+        pass
+    
+    '''
+    Test Post view PUT /authors/uuid/posts/uuid
+    '''
+    def test_put_post_with_id(self):
+        pass
+
+    def test_put_post_without_author_uuid(self):
+        pass
+
+    def test_put_post_with_bad_author_uuid(self):
+        pass
+
+    def test_put_post_with_unknown_author_uuid(self):
+        pass
+
+    def test_put_post_with_missmatched_author_uuid(self):
+        pass
+
+    def test_put_post_without_post_uuid(self):
+        pass
+
+    def test_put_post_with_bad_post_uuid(self):
+        pass
+
+    def test_put_post_with_unknown_post_uuid(self):
+        pass
+
+    def test_put_post_with_missmatched_post_uuid(self):
+        pass
+    
+    '''
+    Test Post view DELETE /authors/uuid/posts/uuid
+    '''
     def test_delete_post(self):
+        pass
+
+    def test_delete_post_without_author_uuid(self):
+        pass
+
+    def test_delete_post_with_bad_author_uuid(self):
+        pass
+
+    def test_delete_post_with_unknown_author_uuid(self):
+        pass
+
+    def test_delete_post_with_missmatched_author_uuid(self):
+        pass
+
+    def test_delete_post_without_post_uuid(self):
+        pass
+
+    def test_delete_post_with_bad_post_uuid(self):
+        pass
+
+    def test_delete_post_with_unknown_post_uuid(self):
+        pass
+
+    def test_delete_post_with_missmatched_post_uuid(self):
         pass
 

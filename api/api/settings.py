3dc99bd9c8bb4b8e167a2c8c85f9404e2ae18859
Adam Ahmed <avahmed@ualberta.ca>
2023-02-24 13:56:48 -0700

    update access token to expire after 1 hour and refresh to expire after 30 days

--- api/api/settings.py
+++ api/api/settings.py
@@ -202,8 +202,8 @@ LOGGING = {
 # Settings for authentication & rest_framework_simplejwt
 # https://django-rest-framework-simplejwt.readthedocs.io/en/latest/settings.html
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=4),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": True,

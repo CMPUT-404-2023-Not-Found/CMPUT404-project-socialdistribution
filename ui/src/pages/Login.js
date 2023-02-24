776b60907242e5b1856ffc420c09feb4784174e2
Adam Ahmed <avahmed@ualberta.ca>
2023-02-24 11:35:37 -0700

    restrucutres input properties to be multiline

--- ui/src/pages/Login.js
+++ ui/src/pages/Login.js
@@ -19,13 +19,23 @@ const Login = () => {
         <h1>Login Page</h1>
         <form action="" method="post">
             <label htmlFor="username">Username </label>
-            <input type="text" id="username" name="post_username" placeholder="Type your username" onChange={(e) => setUsername(e.target.value)}/>
+            <input 
+                type="text" 
+                id="username" 
+                name="username" 
+                placeholder="Type your username" 
+                onChange={(e) => setUsername(e.target.value)}
+                />
             <br></br>
-
             <label htmlFor="password">Password </label>
-            <input type="password" id="password" name="post_password" placeholder="Type your username" onChange={(e) => setPassword(e.target.value)} />
+            <input 
+                type="password" 
+                id="password" 
+                name="password" 
+                placeholder="Type your username" 
+                onChange={(e) => setPassword(e.target.value)} 
+                />
             <br></br>
-
             <button type="button" onClick={login}>Login</button>
             <br></br>
         </form>

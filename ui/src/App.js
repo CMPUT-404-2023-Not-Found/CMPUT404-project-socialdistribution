7567f80c24813576f214af0e7ed739f4df4a64fb
Adam Ahmed <avahmed@ualberta.ca>
2023-02-24 10:54:51 -0700

    Clean up references

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,9 +1,15 @@
-// From
-// https://hygraph.com/blog/routing-in-react
-// App.js
+/*
+2023-02-19
+App.js
 
-// login redirection
-// https://www.youtube.com/watch?v=2k8NleFjG7I
+This code is modified from a tutorial about Routing in React from Joel Olawanle on 2022-09-06, retrieved on 2023-02-19, to hygraph.com
+tutorial here:
+https://hygraph.com/blog/routing-in-react
+
+This code is modified from a tutorial video about React Router V6 from Dennis Ivy on 2022-06-02, retrieved on 2023-02-19, to youtube.com
+tutorial video here:
+https://www.youtube.com/watch?v=2k8NleFjG7I
+*/
 
 import './App.css'
 

114111495ffbfca57a5b65ef63b20ee6fc42e41e
Adam Ahmed <avahmed@ualberta.ca>
2023-02-25 18:25:07 -0700

    remove additional database type

--- api/api/settings.py
+++ api/api/settings.py
@@ -101,15 +101,7 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {}
-if os.getenv("DJANGO_DATABASE_ENGINE_TYPE", 'sqlite') == 'sqlite':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Django Rest Framework (DRF) Configuration
 REST_FRAMEWORK = {

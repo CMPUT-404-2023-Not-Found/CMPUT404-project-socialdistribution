082921dfe4fb843c9a3fae7b5f19cc1a60d8ea32
Adam Ahmed <avahmed@ualberta.ca>
2023-03-03 15:26:17 -0700

    update post base to support new authentication

--- api/post/tests/base.py
+++ api/post/tests/base.py
@@ -5,6 +5,7 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.utils.crypto import get_random_string
 from rest_framework.test import APIClient, APITestCase
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from post.models import Post
 Author = get_user_model()
@@ -13,23 +14,9 @@ class Base(APITestCase):
     '''
     Base class for Post test suite
     '''
-    app_host = 'http://localhost:8000'
-    author_data = {
-        'username': 'testme00',
-        'password': 'P*ssw0rd!',
-        'host': app_host
-    }
+    fixtures = ['fixtures/db.json']
 
-    post_model_data = {
-        'unlisted': False,
-        'visibility': 'PUBLIC',
-        'content': 'Content ' + get_random_string(128),
-        'content_type': 'text/plain',
-        'description': 'Description ' + get_random_string(32),
-        'title': 'Title ' + get_random_string(16)
-    }
-
-    post_api_data = {
+    post_data = {
         'unlisted': False,
         'visibility': 'PUBLIC',
         'content': 'Content ' + get_random_string(128),
@@ -39,38 +26,32 @@ class Base(APITestCase):
     }
 
     def setUp(self):
-        self.client = APIClient()
-        self.author = Author.objects.create(
-            username = 'testme00',
-            password = 'P*ssw0rd!',
-            host = self.app_host
-        )
-        self.author_uuid = str(self.author.id) # type: ignore
-        self.create_post_url = reverse('createPost', kwargs={'author_id': self.author_uuid})
+        self.author = Author.objects.get(username='georgerrmartin')
+        self.author_client = self.configure_client(self.author)
+    
+    def configure_client(self, user):
+        '''
+        Return an APIClient authenticated as user
+        '''
+        client = APIClient()
+        refresh = RefreshToken.for_user(user)
+        client.credentials(HTTP_AUTHORIZATION=f'Bearer {refresh.access_token}')
+        return client
+    
+    def get_create_post_url(self, author_uuid):
+        '''
+        Return string http://sitename/authors/author_uuid/posts/
+        '''
+        return reverse('createPost', kwargs={'author_uuid': str(author_uuid)})
+    
+    def get_list_post_url(self, author_uuid):
+        '''
+        Return string http://sitename/authors/author_uuid/posts/
+        '''
+        return reverse('createPost', kwargs={'author_uuid': str(author_uuid)})
 
-    def create_author(self, **author_data):
-        return Author.objects.create(**author_data)
-    
-    def create_post(self, **post_data):
-        return Post.objects.create(**post_data)
-
-    def get_author_uuid(self, obj):
-        return str(obj.id)
-    
-    def get_create_post_url(self, obj):
+    def get_detail_post_url(self, author_uuid, post_uuid):
         '''
-        Return string http://sitename/authors/author_uuid/posts
+        Return string http://sitename/authors/author_uuid/posts/post_uuid/
         '''
-        if obj:
-            return reverse('createPost', kwargs={'author': str(obj.id)})
-        else:
-            return reverse('createPost', kwargs={'author': str(self.author)})
-    
-    def get_detail_post_url(self, obj, post_uuid):
-        '''
-        Return string http://sitename/authors/author_uuid/posts/post_uuid
-        '''
-        if obj:
-            return reverse('detailPost', kwargs={'author': str(obj.id), 'id': str(post_uuid)})
-        else:
-            return reverse('detailPost', kwargs={'author': str(self.author), 'id': str(post_uuid)})
+        return reverse('detailPost', kwargs={'author_uuid': str(author_uuid), 'id': str(post_uuid)})

bf5809aea3c7d347105484abf5b88dd9c91542fe
Adam Ahmed <avahmed@ualberta.ca>
2023-03-03 15:26:33 -0700

    update test create post to use author_client

--- api/post/tests/test_models.py
+++ api/post/tests/test_models.py
@@ -24,8 +24,9 @@ class PostModelTests(Base):
         '''
         Test legit create post
         '''
-        test_post_data = self.post_api_data
-        response = self.client.post(self.create_post_url, test_post_data)
+        test_post_data = self.post_data
+        create_post_url = self.get_create_post_url(self.author.id)
+        response = self.author_client.post(create_post_url, test_post_data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertDictContainsSubset(test_post_data, response.data) # type: ignore
     

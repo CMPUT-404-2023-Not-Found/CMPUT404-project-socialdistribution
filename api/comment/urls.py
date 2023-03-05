e8d887d6f31f534e2de62b97ad413f393303c01f
Gurveer Sohal <gurveersohal433@gmail.com>
2023-03-05 15:18:21 -0700

    tests pass

--- api/comment/urls.py
+++ api/comment/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import CommentListCreateView, CommentDetailView
 
 urlpatterns = [
-    path('', CommentListCreateView.as_view(), name='createPost'),
+    path('', CommentListCreateView.as_view(), name='comment'),
     path('<uuid:id>/', CommentDetailView.as_view(), name='detailComment')
 ]

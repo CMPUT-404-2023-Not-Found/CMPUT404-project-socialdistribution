77d3da7b9764d845516f2b422de36bdd790ea464
Adam Ahmed <avahmed@ualberta.ca>
2023-03-08 11:25:17 -0700

    add object type requirement in requests

--- api/node/serializers.py
+++ api/node/serializers.py
@@ -5,5 +5,8 @@ from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
 from rest_framework.fields import URLField
 
+from inbox.models import Inbox
+
 class NodeSerializer(serializers.Serializer):
-    url = serializers.URLField(required=True)
+    url     = serializers.URLField(required=True)
+    type    = serializers.ChoiceField(choices=Inbox.TypeChoices.choices, required=True)

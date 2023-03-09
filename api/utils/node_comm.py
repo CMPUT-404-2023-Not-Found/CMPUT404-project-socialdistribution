0748e99c7646d45dc781f0cc5254e41148c926d7
avahmed <avahmed@ualberta.ca>
2023-03-08 17:26:22 -0700

    decode content

--- api/utils/node_comm.py
+++ api/utils/node_comm.py
@@ -54,7 +54,7 @@ class NodeComm():
         node_data = Node.objects.get(host=host_url)
         r = requests.get(object_url, auth=(node_data.username, node_data.password))
         logger.info(r)
-        ret = r.content
+        ret = r.content.decode('utf-8')
         return ret
 
     def parse_object_uuid(self, url):

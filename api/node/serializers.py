6acf6b90088c0ecc898fc483bc8ba623d6ee7f66
Adam Ahmed <avahmed@ualberta.ca>
2023-03-12 14:48:49 -0600

    add author as a valid node-call

--- api/node/serializers.py
+++ api/node/serializers.py
@@ -7,6 +7,10 @@ from rest_framework.fields import URLField
 
 from inbox.models import Inbox
 
+# Inherit choices from Inbox defaults then add author
+typeChoices = Inbox.TypeChoices.choices
+typeChoices.append(('author', 'Author'))
+
 class NodeSerializer(serializers.Serializer):
     url     = serializers.URLField(required=True)
-    type    = serializers.ChoiceField(choices=Inbox.TypeChoices.choices, required=True)
+    type    = serializers.ChoiceField(choices=typeChoices, required=True)

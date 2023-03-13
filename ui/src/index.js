5c6d71266919a168f10594988ff70787235fa70a
Adam Ahmed <avahmed@ualberta.ca>
2023-03-13 11:36:13 -0600

    install & setup material ui

--- ui/src/index.js
+++ ui/src/index.js
@@ -8,9 +8,14 @@ https://hygraph.com/blog/routing-in-react
 */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

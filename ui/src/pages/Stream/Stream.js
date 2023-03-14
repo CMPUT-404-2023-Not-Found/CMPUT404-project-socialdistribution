9c3e8bd37e8f3e5a87c5ab76682b0e33c2a6439f
Adam Ahmed <avahmed@ualberta.ca>
2023-03-13 20:55:00 -0600

    remove old navbar & init stream page

---
+++ ui/src/pages/Stream/Stream.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Grid from '@mui/material/Grid';
+
+const Stream = () => {
+    return (
+        <Grid item xs={8}>
+            This is your stream.
+        </Grid>
+    );
+}
+
+export default Stream;

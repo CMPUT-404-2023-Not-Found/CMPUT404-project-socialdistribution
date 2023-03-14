16778e37e93b266ad12793f3f8177f9c5fa430d4
Adam Ahmed <avahmed@ualberta.ca>
2023-03-13 20:54:46 -0600

    migrate router to index

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,26 +2,48 @@
 2023-02-19
 index.js
 
-This code is from a tutorial about Routing in React from Joel Olawanle on 2022-09-06, retrieved on 2023-02-19, to hygraph.com
+This code is modified from a tutorial about Routing in React from Joel Olawanle on 2022-09-06, retrieved on 2023-02-19, to hygraph.com
 tutorial here:
 https://hygraph.com/blog/routing-in-react
 */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-
-import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+
+import App from './App';
+import { AuthProvider } from './context/AuthContext';
+import PrivateRoutes from './utils/PrivateRoutes'
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Stream from './pages/Stream/Stream';
+import Posts from './pages/Posts';
+import PostDetail from './components/PostDetail';
+import NotFound from './components/NotFound';
+import CreatePost from './pages/CreatePost';
+
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-   <React.StrictMode>
-      <BrowserRouter>
-         <App />
-      </BrowserRouter>
-   </React.StrictMode>
+   <BrowserRouter>
+      <AuthProvider>
+         <Routes>
+            <Route path='/' element={<App />}>
+               <Route element={<PrivateRoutes />}>
+                  <Route path="/" element={<Stream/>} exact/>
+                  <Route path="/posts" element={<Posts />} />
+                  <Route path="/posts/:postid" element={<PostDetail />} />
+                  <Route path="/profile" element={<Profile />} />
+                  <Route path="/createpost" element={<CreatePost />} />
+               </Route>
+               <Route path="/login" element={<Login />} />
+               <Route path="*" element={<NotFound />} />
+            </Route>
+         </Routes>
+      </AuthProvider>
+   </BrowserRouter>
 );

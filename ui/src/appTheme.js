c9297de18fae57cdd6637344113b85e009276873
Adam Ahmed <avahmed@ualberta.ca>
2023-03-14 12:36:09 -0600

    move typography to appTheme

--- ui/src/appTheme.js
+++ ui/src/appTheme.js
@@ -2,8 +2,18 @@ import { createTheme } from '@mui/material/styles';
 
 export const appTheme = createTheme({
     palette: {
+        mode: 'light',
         primary: {
-            main: '#265156',
-        }
-    }
+          main: '#265156',
+        },
+        secondary: {
+          main: '#562651',
+        },
+        background: {
+          default: '#efefef',
+        },
+      },
+      typography: {
+        fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+      },
 });

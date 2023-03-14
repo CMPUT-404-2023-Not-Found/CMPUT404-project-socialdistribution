fbbdab04b6d7c8f8580cfb5d162e69329bf50011
Adam Ahmed <avahmed@ualberta.ca>
2023-03-13 21:09:20 -0600

    update navbaritem to logout user when click logout

--- ui/src/components/Navbar/Navbar.js
+++ ui/src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useContext } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Drawer from '@mui/material/Drawer';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
@@ -9,15 +9,12 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 
 import AuthContext from '../../context/AuthContext';
 import { mainNavbarItems, secondaryNavbarItems } from './consts/navbarItems';
 import { navbarStyles } from './consts/styles';
 
 const Navbar = () => {
-    const drawerWidth = 220;
     let {user, logoutUser} = useContext(AuthContext);
     const navigate = useNavigate();
 
@@ -45,13 +42,16 @@ const Navbar = () => {
             <Divider />
             <List>
                 {secondaryNavbarItems.map((item, index) => (
-                <ListItem key={item.id} onClick={() => navigate(item.route)} disablePadding>
-                    <ListItemButton>
-                    <ListItemIcon sx={navbarStyles.icons}>
-                        {item.icon}
-                    </ListItemIcon>
-                    <ListItemText primary={item.label} sx={navbarStyles.text} />
-                    </ListItemButton>
+                <ListItem 
+                    key={item.id} 
+                    onClick={item.label === 'Logout' ? logoutUser : () => navigate(item.route)} 
+                    disablePadding>
+                        <ListItemButton>
+                        <ListItemIcon sx={navbarStyles.icons}>
+                            {item.icon}
+                        </ListItemIcon>
+                        <ListItemText primary={item.label} sx={navbarStyles.text} />
+                        </ListItemButton>
                 </ListItem>
                 ))}
             </List>

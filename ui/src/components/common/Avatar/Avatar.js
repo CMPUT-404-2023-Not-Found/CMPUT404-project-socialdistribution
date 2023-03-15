a7ba794fc6c429010e7d6f51d359d26b2e28106a
Adam Ahmed <avahmed@ualberta.ca>
2023-03-15 17:33:48 -0600

    init avatar components

---
+++ ui/src/components/common/Avatar/Avatar.js
@@ -0,0 +1,14 @@
+/*
+2023-03-15
+ui/src/components/common/Avatar/Avatar.js
+
+*/
+import React from 'react'
+
+const Avatar = ({ author }) => {
+  return (
+    <div>Avatar</div>
+  )
+}
+
+export default Avatar

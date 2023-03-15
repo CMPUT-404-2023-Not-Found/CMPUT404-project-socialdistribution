22314a56cddd49d61c103a3b14d47718eca11ed0
Jorge Marquez Peralta <marquezp@ualberta.ca>
2023-03-15 16:31:50 -0600

    basic serializer for follower

--- api/follower/serializers.py
+++ api/follower/serializers.py
@@ -6,16 +6,48 @@ from rest_framework import serializers
 from rest_framework.fields import ChoiceField, DateTimeField, IntegerField, URLField, UUIDField
 
 from author.serializers import ExistingAuthorSerializer
-from .models import Follower
+from .models import Follower, Author
+from urllib.parse import urlparse
+from django.conf import settings
 
 logger = logging.getLogger('django')
-rev = 'rev: $xani93n$x' # TODO ??
+rev = 'rev: $xani93n$x'
 
 # This code is modifed from a video tutorial from Cryce Truly on 2020-06-19 retrieved on 2023-02-16, to Youtube crycetruly
 # video here:
 # https://youtu.be/B3HGwFlBvi8
 class FollowerSerializer(serializers.ModelSerializer):
-    follower = URLField(required = False)
+    # TODO QUESTION: Can I change the Author serializer to match the spec? Some of the fields are out of order
+    # TODO Does this even matter? ^
+    
+    follower = serializers.SerializerMethodField('get_follower')
+
+    def get_follower(self, obj):
+        url = str(obj.follower)
+        parsedURL = urlparse(url)
+
+        hostname = parsedURL.hostname
+        port = parsedURL.port
+
+        if f'http://{hostname}:{port}' == str(settings.APP_URL):
+            # post = PostSerializer(obj.post)
+            # return post.data['author']
+            # idk which way is best
+
+            path = parsedURL.path.split('/')
+            if path[-1] == '':
+                path.pop()
+            
+            author_uuid = path[-1]
+            author_obj = Author.objects.get(id=author_uuid)
+            author_obj = ExistingAuthorSerializer(author_obj, read_only=True)
+            return author_obj.data
+        else:
+            # make call to the author url
+            pass
+        return str(obj.author)
+    type            = serializers.SerializerMethodField('get_type')
+    def get_type(self, obj): return 'followers'
     class Meta:
         model = Follower
-        fields = ['follower']
+        fields = ['type','follower']
--- api/follower/views.py
+++ api/follower/views.py
@@ -20,32 +20,23 @@ class FollowerListView(ListAPIView):
     queryset = Follower.objects.all()
 
 class FollowerDetailView(RetrieveUpdateDestroyAPIView):
-    #serializer_class = FollowerSerializer
+    serializer_class = FollowerSerializer
     queryset = Follower.objects.all()
     lookup_field = 'follower'
-    
-    '''
-    def put(self, request, *args, **kwargs):
+    # TODO QUESTION: we don't use the serializer for PUT, but we need it for GETing a specific follower?
+    # TODO QUESTION: How do we implement the "request" part of this? New app? 
+    def get_object(self):
         logger.info(rev)
-        follower_data = {
-            'follower': kwargs.get('follower','')
-        }
-        logger.info(follower_data)
-        serializer = FollowerSerializer(data=request.data)
-        if serializer.is_valid():
-            logger.info('its valid')
-
-        else:
-            logger.info('not valid :()')
-        return super().put(request, *args, **kwargs)
-        '''
+        follower = self.kwargs.get(self.lookup_field)
+        logger.info('Getting content for follower: [%s]', follower)
+        return super().get_object()
     
     def put(self, request, *args, **kwargs):
         # get author(followee) uuid
         followee = Author.objects.get(id=kwargs['author_uuid'])
         # get follower url
         follower_url = kwargs['follower']
-        # TODO check that author doesnt already follow the followee
+        # check that author doesnt already follow the followee
         exists = Follower.objects.filter(followee=followee, follower=follower_url)
         # save entry to database
         if exists:
@@ -59,35 +50,8 @@ class FollowerDetailView(RetrieveUpdateDestroyAPIView):
             else:
                 return Response('error',status=status.HTTP_404_NOT_FOUND)
         
-    #TODO Implement delete
-    def delete():
-        pass
-    
-def index(request):
-    # current_viewed_author: the author who is being viewed by the current user
-    current_viewed_author = request.GET.get('author')
-    # current_author: the author who is currently logged in
-    current_logged_author = request.user.username
-    # current_viewed_author_follwers: the list of authors that are following the currently being viewed author
-    current_viewed_author_follwers = FollowerCount.objects.filter(followee=current_viewed_author)
-    # current_viewed_author_follwers_count: the number of followers of the currently being viewed author
-    current_viewed_author_follwers_count = len(current_viewed_author_follwers)
-    # current_logged_author_followers: the list of authors that the currently logged author is following
-    current_logged_author_follwings = FollowerCount.objects.filter(follwer=current_logged_author)
-    # current_logged_author_follwing_count: the number of authors that the currently logged author is following 
-    current_logged_author_follwing_count = len(current_logged_author_follwings)
-    return render(request, 'follower/index.html', 
-                  {'current_viewed_author': current_viewed_author, 
-                   'current_viewed_author_follwers': current_viewed_author_follwers_count,
-                   'current_author_follwing_count': current_logged_author_follwing_count
-                   })
-    
-def follwers_count(request):
-    if request.method == "POST":
-        value = request.POST["value"]
-        author = request.POST["author"] # the followee author 
-        follwer = request.POST["follwer"] # the follower author
-        if value == "follow": # if the follwer is following the followee
-            follwers_count = FollowerCount.objects.create(follwer=follwer, author=author)
-            follwers_count.save()
-        return redirect("/" + author.__str__)  
+    def perform_destroy(self, instance):
+        logger.info(rev)
+        follower = self.kwargs.get(self.lookup_field)
+        logger.info('Deleting follower: [%s]', follower )
+        return super().perform_destroy(instance)

2d4be1b9f8227eec2aafd183c20f47e96fd46450
Adam Ahmed <avahmed@ualberta.ca>
2023-03-15 11:34:52 -0600

    cleanup 404 not found page

--- ui/src/components/NotFound.js
+++ ui/src/components/NotFound.js
@@ -1,21 +1,28 @@
+/*
+2023-02-20
+ui/src/components/NotFound.js
+
+*/
+
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Button, CardContent, CardHeader, Typography } from '@mui/material';
+
+import BasicCard from '../components/common/BasicCard/BasicCard';
 import GridWrapper from './common/GridWrapper/GridWrapper';
 
 const NoMatch = () => {
     let navigate = useNavigate();
-
     return (
         <GridWrapper>
-            <div>
-                404 page not found
-            </div>
-
-            <button onClick={() => {
-                navigate('/')
-            }}>
-                go back to stream
-            </button>
+            <BasicCard
+                header={<CardHeader title='404' subheader='Page Not Found' />}
+                content={
+                    <CardContent>
+                        <Button variant='contained' onClick={() => {navigate('/')}}>Go Back To Stream</Button>
+                    </CardContent>
+                }
+            />
         </GridWrapper>
     );
 }

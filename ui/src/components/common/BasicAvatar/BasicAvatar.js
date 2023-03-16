96a0323352d649497326bae9e44e35bb7b0515fc
Adam Ahmed <avahmed@ualberta.ca>
2023-03-16 11:12:07 -0600

    move styling to seperate file

--- ui/src/components/common/BasicAvatar/BasicAvatar.js
+++ ui/src/components/common/BasicAvatar/BasicAvatar.js
@@ -6,16 +6,19 @@ ui/src/components/common/Avatar/Avatar.js
 import React from 'react'
 import Avatar from '@mui/material/Avatar';
 
-const BasicAvatar = ({ profile }) => {
+import { avatarStyles } from './styles';
+
+const BasicAvatar = ({ profile, size }) => {
     const authorName = profile.displayName;
-    const authorNameShort = authorName.charAt(0);
+    const authorNameShort = (authorName ? authorName.charAt(0) : 'h');
+    const avatarSize = (size ? size : 'small');
     if (profile.profileImage) {
         return (
-        <Avatar alt={authorName} src={profile.profileImage} sx={{ width: 128, height: 128 }}></Avatar>
+        <Avatar alt={authorName} src={profile.profileImage} sx={avatarStyles[avatarSize]}></Avatar>
         );
     } else {
         return (
-        <Avatar sx={{ bgcolor: 'primary.main', width: 128, height: 128 }}>{authorNameShort}</Avatar>
+        <Avatar sx={avatarStyles[avatarSize]}>{authorNameShort}</Avatar>
         );
     }
 }

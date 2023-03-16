c24fe6be8a2ad555ad8133332455ff76adf90e67
MichaeLsh <lsh20011018@gmail.com>
2023-03-15 20:47:02 -0600

    "follwer/tests.py init"

--- api/follower/tests.py
+++ api/follower/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+# 2023-03-15
+# follower/tests.py
 
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient, APITestCase
+
+from author.models import Author
+from follower.models import Follower
 # Create your tests here.
+
+class FollowerTestCase(APITestCase):
+    """
+    Test cases for Follower model
+    """
+    
+    """
+    - Add a follower to an author
+    - Remove a follower from an author
+    - Get a list of followers for an author
+    
+    """

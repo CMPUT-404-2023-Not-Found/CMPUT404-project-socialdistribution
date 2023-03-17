ace555d029bdb0429abff25f45ce64f76bb9015b
Adam Ahmed <avahmed@ualberta.ca>
2023-03-17 12:01:53 -0600

    fix up comment scripts

--- bin/api/crt-comment.sh
+++ bin/api/crt-comment.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # 2023-03-04
 # crt-comment.sh
 # Create a comment for a post by an author
@@ -8,20 +8,11 @@ source .env
 if [ "#$APP_URL" = "#" ]; then echo "ERR Could could not find $APP_URL in env, is your env setup?"; exit 1; fi
 if [ "#$1" = "#" -o "#$2" = "#" ]; then echo "Usage $0 [author_uuid] [post_uuid]"; exit 1; fi
 author_uuid="$1"
+auth_hdr=$(./get-auth.sh 'bearer' $(cat .username) $(cat .password))
 post_uuid="$2"
-
-if [ ! -r .access_token ]
-then
-    echo "ERR Could not find a .access_token file, try using ./get-token.sh [username] [password]"
-    exit 1
-fi
-
-access_token=`cat .access_token`
-auth_hdr="Authorization: Bearer $access_token"
-
 crt_comment_url=`printf "${COMMENT_API}/" $author_uuid $post_uuid`
-rand=`echo $RANDOM | md5sum | head -c 8`
 
+rand=`echo $RANDOM | md5sum | head -c 8`
 crt_body=`cat <<EOF
 {
   "comment" : "$rand Content",

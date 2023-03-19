9421f21c7bdb258fdf4f9a1ac58834510d53d382
Adam Ahmed <avahmed@ualberta.ca>
2023-03-18 20:55:36 -0600

    improve inbox admin

--- api/inbox/admin.py
+++ api/inbox/admin.py
@@ -7,6 +7,12 @@ from .models import Inbox
 
 class InboxAdmin(admin.ModelAdmin):
     model = Inbox
-    list_display = ('author', 'summary', 'sender_author_id', 'type', 'received_at')
+    def inbox_owner(self, obj):
+        return obj.author.display_name if obj.author.display_name else obj.author.username
+    inbox_owner.short_description = 'Inbox Owner'
+
+    list_display = ('inbox_owner', 'summary', 'sender_author_id', 'type', 'received_at')
     list_filter = ('author', )
+    search_fields = ('summary', 'sender_author_id')
+    ordering = ('id', 'received_at')
 admin.site.register(Inbox, InboxAdmin)

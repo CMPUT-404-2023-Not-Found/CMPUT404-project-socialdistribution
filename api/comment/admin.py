87108b27537afe70153c994d1d3bec1f441514d6
Adam Ahmed <avahmed@ualberta.ca>
2023-03-18 21:20:36 -0600

    include more features

--- api/comment/admin.py
+++ api/comment/admin.py
@@ -12,13 +12,17 @@ class CommentAdmin(admin.ModelAdmin):
         return obj.post.get_node_id()
     post_id.short_description = 'Parent Post Id'
 
+    def post_title(self, obj):
+        return obj.post.title
+    post_title.short_description = 'Parent Post Title'
+
     def node_id(self, obj):
         return obj.get_node_id()
     node_id.short_description = 'Node Id'
 
-    list_display = ('node_id', 'comment', 'content_type', 'post_id', 'author', 'published')
-    list_filter = ('post', 'author', 'content_type')
-    search_fields = ('id', 'author', 'comment')
+    list_display = ('node_id', 'comment', 'content_type', 'post_id', 'post_title', 'author', 'published')
+    list_filter = ('post__title', 'author', 'content_type')
+    search_fields = ('id', 'author', 'comment', 'post__title')
     ordering = ('id', )
 
 admin.site.register(Comment, CommentAdmin)

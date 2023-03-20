246846253d9f9949ff880e4dc8b52155d204d661
Adam Ahmed <avahmed@ualberta.ca>
2023-03-19 19:13:22 -0600

    compress serializers

--- api/node/serializers.py
+++ api/node/serializers.py
@@ -13,13 +13,3 @@ class NodeRetrieveSerializer(serializers.Serializer):
     url     = serializers.URLField(required=True)
     type    = serializers.ChoiceField(choices=typeChoices, required=True)
 
-class NodeSendSerializer(serializers.Serializer):
-    context = serializers.ChoiceField(choices=Inbox.W3ContextChoices.choices, default=Inbox.W3ContextChoices.W3_AS)
-    summary = serializers.CharField(max_length=128)
-    type    = serializers.ChoiceField(choices=Inbox.TypeChoices.choices, required=True)
-    object  = serializers.URLField(max_length=256, min_length=None, allow_blank=False)
-
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation['@context'] = representation.pop('context')
-        return representation

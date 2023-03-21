3136a5963d59bc7b3f1b5191fe902ad55d9e6b5a
Adam Ahmed <avahmed@ualberta.ca>
2023-03-21 11:01:47 -0600

    add menu items to notification for page header

--- ui/src/components/Page/PageHeader.js
+++ ui/src/components/Page/PageHeader.js
@@ -7,12 +7,19 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
+import { menuItems } from './notificationMenuItems';
+import NotificationBell from '../common/NotificationBell/NotificationBell';
 import { pageStyles } from './styles';
 
-const PageHeader = ({ title, profile }) => {
-  return (
+const PageHeader = ({ title, disableNotification=false }) => {
+    
+    // RENDER APP =================================================
+    return (
     <Box sx={pageStyles.PageHeader.wrapper}>
-        <Box sx={pageStyles.PageHeader.row}>
+        <Box sx={pageStyles.PageHeader.topRow}>
+        { !disableNotification && <NotificationBell iconColor='secondary' badgeContent={4} menuItems={menuItems}/> }
+        </Box>
+        <Box sx={pageStyles.PageHeader.middleRow}>
             <Typography variant='h4' color='white'>{title}</Typography>
         </Box>
     </Box>

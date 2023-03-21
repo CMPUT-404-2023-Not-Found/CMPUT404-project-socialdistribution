9e53ecba91608bf956bd40e8dc6071f55d054a28
Adam Ahmed <avahmed@ualberta.ca>
2023-03-21 13:08:33 -0600

    add ping that updates notifications every fifthteen minutes

--- ui/src/components/Page/PageHeader.js
+++ ui/src/components/Page/PageHeader.js
@@ -3,21 +3,46 @@
 ui/src/components/Page/PageHeader.js
 
 */
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
+import AuthContext from '../../context/AuthContext';
+import Backend from '../../utils/Backend';
 import { menuItems } from './notificationMenuItems';
 import NotificationBell from '../common/NotificationBell/NotificationBell';
 import { pageStyles } from './styles';
 
 const PageHeader = ({ title, disableNotification=false }) => {
-    
+    const [ notification, setNotification ] = useState(0);
+    const { user, authTokens, logoutUser } = useContext(AuthContext);
+
+    //  event listners --------------------------------------------
+    useEffect(() => {
+        const getNotifications = async () => {
+            const [response, data] = await Backend.get(`/api/authors/${user.user_id}/inbox/?count`, authTokens.access);
+            if (response.status && response.status === 200) {
+                console.log('Got notifications data: ');
+                console.log(data);
+                data.count && setNotification(data.count);
+            } else if (response.statusText === 'Unauthorized'){
+                logoutUser();
+            } else {
+                console.log('Failed to get notifications');
+            }
+        }
+        const fifthteenMinutes = 15 * 60 * 1000;
+        let interval = setInterval(() => {
+            getNotifications();
+        }, fifthteenMinutes);
+        return () => clearInterval(interval);
+    }, [notification])
+
     // RENDER APP =================================================
     return (
     <Box sx={pageStyles.PageHeader.wrapper}>
         <Box sx={pageStyles.PageHeader.topRow}>
-        { !disableNotification && <NotificationBell iconColor='secondary' badgeContent={4} menuItems={menuItems}/> }
+        { !disableNotification && <NotificationBell iconColor='secondary' badgeContent={notification} menuItems={menuItems}/> }
         </Box>
         <Box sx={pageStyles.PageHeader.middleRow}>
             <Typography variant='h4' color='white'>{title}</Typography>

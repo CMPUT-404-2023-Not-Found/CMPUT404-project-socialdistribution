76a69cec61f7683bd3cbc3b599faf7016c357a60
Adam Ahmed <avahmed@ualberta.ca>
2023-03-21 11:01:28 -0600

    init basic menu on notification

---
+++ ui/src/components/common/BasicMenu/BasicMenu.js
@@ -0,0 +1,35 @@
+/*
+2023-03-21
+ui/src/components/common/BasicMenu/BasicMenu.js
+
+This code is from a github example of working with MUI by theatypicaldeveloper, last commited 2021-12-22, retrieved on 2023-03-21
+github source code here:
+https://github.com/theatypicaldeveloper/materialUi-in-react/blob/lesson-4-common-components-completed/src/components/common/BasicMenu/BasicMenu.js
+*/
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
+const BasicMenu = ({ anchorEl, handleClose, open, menuItems }) => {
+    const navigate = useNavigate();
+
+    return (
+        <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+        >
+        {menuItems.map((item) => (
+          <MenuItem
+            onClick={() => { navigate(item.route); handleClose() }}
+          >
+            {item.label}
+          </MenuItem>
+        ))}
+        </Menu>
+    );
+}
+
+export default BasicMenu;
--- ui/src/components/common/NotificationBell/NotificationBell.js
+++ ui/src/components/common/NotificationBell/NotificationBell.js
@@ -1,22 +1,46 @@
 /*
 2023-03-21
+ui/src/components/common/NotificationBell/NotificationBell.js
 
-This code is modified from a github example of a notification bell in MUI by theatypicaldeveloper, last commited 2021-12-22, retrieved on 2023-03-21
+This code is from a github example of working with MUI by theatypicaldeveloper, last commited 2021-12-22, retrieved on 2023-03-21
 github source code here:
 https://github.com/theatypicaldeveloper/materialUi-in-react/blob/lesson-4-common-components-completed/src/components/common/NotificationBell/NotificationBell.js
 */
-import React from 'react';
+import React, { useState } from 'react';
 import Badge from '@mui/material/Badge'
 import IconButton from '@mui/material/IconButton';
 import NotificationsIcon from '@mui/icons-material/Notifications';
-import { Tooltip } from '@mui/material';
+import Tooltip from '@mui/material/Tooltip';
 
-const NotificationBell = ({ iconColor, badgeContent }) => {
+import BasicMenu from '../BasicMenu/BasicMenu';
+
+const NotificationBell = ({ iconColor, badgeContent, menuItems }) => {
+    //  variable declarations -------------------------------------
     const newNotification = `You have ${badgeContent} new notifications`;
     const noNewNotification = 'No new notifications';
+    const [ open, setOpen ] = useState(false);
+    const [ anchorEl, setAnchorEl ] = useState(null);
+
+    //  event listners --------------------------------------------
+    //  async functions -------------------------------------------
+    //  functions -------------------------------------------------
+    const handleOpen = (e) => {
+        setAnchorEl(e.currentTarget);
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
+            <>
             <Tooltip title={badgeContent ? newNotification : noNewNotification}>
-                <IconButton color={iconColor}>
+                <IconButton 
+                    color={iconColor}
+                    onClick={badgeContent ? handleOpen : null}
+                    anchorEl={anchorEl}
+                >
                     <Badge
                         badgeContent={badgeContent}
                         color='error'
@@ -25,6 +49,13 @@ const NotificationBell = ({ iconColor, badgeContent }) => {
                     </Badge>
                 </IconButton>
             </Tooltip>
+            <BasicMenu
+                open={open}
+                anchorEl={anchorEl}
+                handleClose={handleClose}
+                menuItems={menuItems}
+            />
+            </>
   );
 }
 

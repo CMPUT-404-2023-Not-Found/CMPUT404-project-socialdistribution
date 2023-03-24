759928c75bbd70103dc5a429d5b6cf5a8920c6d4
MichaeLsh <lsh20011018@gmail.com>
2023-03-23 23:44:13 -0600

    edit post page progress saved

---
+++ ui/src/pages/EditPost.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useContext, useState, useEffect } from 'react';
+import AuthContext from '../context/AuthContext';
+import DynamicForm from '../utils/DynamicForm';
+import Backend from '../utils/Backend';
+import {useForm} from 'react-hook-form';
+
+import GridWrapper from '../components/common/GridWrapper/GridWrapper';
+import PageHeader from '../components/Page/PageHeader';
+
+const EditPost = () => {
+    
+}

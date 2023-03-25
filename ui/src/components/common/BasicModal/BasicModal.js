e0ec0fb582c620ce9c658b9a54a47b39d1a4b253
Adam Ahmed <avahmed@ualberta.ca>
2023-03-25 10:07:44 -0600

    move styles out & add placeholder inputs

--- ui/src/components/common/BasicModal/BasicModal.js
+++ ui/src/components/common/BasicModal/BasicModal.js
@@ -2,35 +2,44 @@
 2023-03-24
 ui/src/components/common/BasicModal/BasicModal.js
 
+This code is modified from a video tutorial of using MUI by theatypicaldeveloper, uploaded 2022-01-09, retrieved on 2023-03-25 from youtube.com
+youtube video here:
+https://youtu.be/4h-VWmlfJh4
 */
 
 import React from 'react';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
+import Input from '@mui/material/Input';
 import Typography from '@mui/material/Typography';
 
+import { modalStyles } from './styles';
+import CommonButton from '../CommonButton/CommonButton';
+
 const BasicModal = ({ open, onClose }) => {
-    const style = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 400,
-        bgcolor: 'background.paper',
-        border: '2px solid #000',
-        boxShadow: 24,
-        p: 4,
-      };
+    const validate = () => {
+
+    };
+
     return (
         <Modal open={open} onClose={onClose}>
-            <Box sx={style}>
-                <Typography id="spring-modal-title" variant="h6" component="h2">
-                Text in a modal
+            <Box sx={modalStyles.wrapper}>
+                <Typography variant="h6" component="h2">
+                    Share with followers
                 </Typography>
-                <Typography id="spring-modal-description" sx={{ mt: 2 }}>
-                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                <Typography sx={{ mt: 2 }}>
+                    Choose which followers to share with
                 </Typography>
+                <Box sx={modalStyles.inputFields}>
+                    <Input placeholder='this is a placeholder'></Input>
+                    <Input placeholder='this is a placeholder'></Input>
+                    <Input placeholder='this is a placeholder'></Input>
+                </Box>
+                <Box sx={modalStyles.buttons}>
+                    <CommonButton variant='contained' onClick={validate}>Submit</CommonButton>
+                    <CommonButton onClick={onClose}>Cancel</CommonButton>
+                </Box>
             </Box>
         </Modal>
     );

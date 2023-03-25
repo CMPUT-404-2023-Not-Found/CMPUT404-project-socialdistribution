89f289d2055ff1b603f903fd58fbe42bf28c1cdd
Adam Ahmed <avahmed@ualberta.ca>
2023-03-25 14:08:26 -0600

    init PostCard

---
+++ ui/src/components/Post/PostCard.js
@@ -0,0 +1,33 @@
+/*
+2023-03-25
+ui/src/components/Post/PostCard.js
+
+*/
+
+import React from 'react';
+
+import BasicCard from '../common/BasicCard/BasicCard';
+import PostHeader from './PostHeader';
+import PostContent from './PostContent';
+
+const PostCard = ({ post, actions }) => {
+    return (
+        <BasicCard
+            header={
+            <PostHeader
+                title={post.title ? post.title : 'No title'}
+                author={post.author && post.author}
+                time={post.updated_at ? post.updated_at : post.published}
+            />}
+            content={
+            <PostContent 
+                description={post.description}
+                contentType={post.contentType}
+                content={post.content}
+                postNodeId={post.id}
+            />}
+        />
+    );
+}
+
+export default PostCard;

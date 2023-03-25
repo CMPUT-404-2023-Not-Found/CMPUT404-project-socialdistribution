416c9c8f782151fb9edfa29359fbc923ac6c02e6
Adam Ahmed <avahmed@ualberta.ca>
2023-03-25 14:48:35 -0600

    remove unnecessary code

--- ui/src/components/Post/PostContent.js
+++ ui/src/components/Post/PostContent.js
@@ -4,39 +4,15 @@ ui/src/components/Post/PostContent.js
 
 */
 
-import React, { useState } from 'react'
-import { styled } from '@mui/material/styles';
-import CardActions from '@mui/material/CardActions';
+import React from 'react'
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Collapse from '@mui/material/Collapse';
 import Divider from '@mui/material/Divider';
 import Typography from '@mui/material/Typography';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import IconButton from '@mui/material/IconButton';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ReactMarkdown from 'react-markdown'
 
 import PostActions from './PostActions';
-import ShareWithFollower from '../Modals/ShareWithFollower/ShareWithFollower';
 import { PostContentStyles } from './styles';
-
-/*
-This code is modified from a documentation guide on Material UI Card components from Material UI SAS 2023, retrieved 2023-03-13 from mui.com
-guide here
-https://mui.com/material-ui/react-card/#complex-interaction
-*/
-const ExpandMore = styled((props) => {
-    const { expand, ...other } = props;
-    return <IconButton {...other} />;
-    })(({ theme, expand }) => ({
-        transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-        marginLeft: 'auto',
-        transition: theme.transitions.create('transform', {
-            duration: theme.transitions.duration.shortest,
-    }),
-}));
 
 const PostContent = ({ description, contentType, content, postNodeId }) => {
 

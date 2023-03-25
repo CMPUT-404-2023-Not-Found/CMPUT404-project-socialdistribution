015c4bde9aae0e34c90aa39eb3dda06b0c3396fa
Adam Ahmed <avahmed@ualberta.ca>
2023-03-25 11:57:30 -0600

    get list of followers

--- ui/src/components/Modals/ShareWithFollower/ShareWithFollower.js
+++ ui/src/components/Modals/ShareWithFollower/ShareWithFollower.js
@@ -7,19 +7,44 @@ youtube video here:
 https://youtu.be/4h-VWmlfJh4
 */
 
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 
+import AuthContext from '../../../context/AuthContext';
+import Backend from '../../../utils/Backend';
 import BasicModal from '../../common/BasicModal/BasicModal';
 import CommonButton from '../../common/CommonButton/CommonButton';
 import { modalStyles } from './styles';
 
 const ShareWithFollower = ({ open, onClose }) => {
+    //  variable declarations -------------------------------------
     const [ followerList, setFollowerList ] = useState([]);
     const [ sendList, setSendList ] = useState({});
-    
+    const { user, authTokens, logoutUser } = useContext(AuthContext);
+
+    //  event listeners -------------------------------------------
+    useEffect(() => {
+      if (open) { 
+        setSendList({});
+        const getFollowers = async () => {
+            const [response, data] = await Backend.get(`/api/authors/${user.user_id}/followers/`, authTokens.access);
+            if (response.status && response.status === 200) {
+                console.log('Got followers');
+                console.log(data);
+                setFollowerList(data.items);
+            } else if (response.statusText === 'Unauthorized'){
+                logoutUser();
+            } else {
+                console.log('Failed to get followers');
+            }
+        };
+        getFollowers();
+        }
+    }, [open, user, authTokens, logoutUser]);
+
+    //  functions       -------------------------------------------
     const handleChange = (e) => {
         const follower_id = e.target.value;
         const send_to_follower = e.target.checked;
@@ -32,11 +57,7 @@ const ShareWithFollower = ({ open, onClose }) => {
         onClose();
     };
 
-    useEffect(() => {
-      if (open) setSendList({});
-    }, [open]);
-    
-
+    // RENDER APP =================================================
     const renderContent = () => {
         return (
         <form>

7f8ecfbb58c573decdaa75930bc35c83a2d82ada
Adam Ahmed <avahmed@ualberta.ca>
2023-03-25 15:24:36 -0600

    add tooltip to share

--- ui/src/components/Actions/ShareAction/ShareAction.js
+++ ui/src/components/Actions/ShareAction/ShareAction.js
@@ -6,6 +6,7 @@ ui/src/components/Actions/ShareAction/ShareAction.js
 import React, { useState } from 'react';
 import IconButton from '@mui/material/IconButton';
 import ShareIcon from '@mui/icons-material/Share';
+import Tooltip from '@mui/material/Tooltip';
 
 import ShareModal from '../../Modals/ShareModal/ShareModal'
 
@@ -16,9 +17,11 @@ const ShareAction = ({ objectNodeId }) => {
     };
     return (
         <>
+        <Tooltip title='Share with your followers'>
         <IconButton aria-label="share" onClick={onClickShare}>
                 <ShareIcon />
-            </IconButton>
+        </IconButton>
+        </Tooltip>
         <ShareModal open={open} onClose={() => setOpen(false)} objectNodeId={objectNodeId} />
         </>
     );

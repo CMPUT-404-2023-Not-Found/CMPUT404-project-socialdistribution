292b0358c43143fe42bd6cc7083aeb3060ea9e0f
Adam Ahmed <avahmed@ualberta.ca>
2023-03-25 14:48:18 -0600

    use PostCard

--- ui/src/pages/Stream/Stream.js
+++ ui/src/pages/Stream/Stream.js
@@ -11,8 +11,7 @@ import Backend from '../../utils/Backend';
 import AuthContext from '../../context/AuthContext';
 import BasicCard from '../../components/common/BasicCard/BasicCard';
 import GridWrapper from '../../components/common/GridWrapper/GridWrapper';
-import PostHeader from '../../components/Post/PostHeader';
-import PostContent from '../../components/Post/PostContent';
+import PostCard from '../../components/Post/PostCard';
 import PageHeader from '../../components/Page/PageHeader';
 
 const Stream = () => {
@@ -38,29 +37,11 @@ const Stream = () => {
 
     // RENDER APP =================================================
     const renderNodePosts = (items) => {
-        // console.log(items);
         if (!items || items.length <= 0) return (<Typography paragraph >No Posts</Typography>);
         let itemsRender = [];
         items.forEach((item, idx) => {
             console.log(item);
-            itemsRender.push(
-                <BasicCard 
-                    key={idx}
-                    header={
-                        <PostHeader 
-                            author={item.author} 
-                            title={item.title} 
-                            time={(item.updated_at ? item.updated_at : item.published)} 
-                        />}
-                    content={
-                        <PostContent 
-                            description={item.description}
-                            contentType={item.contentType}
-                            content={item.content}
-                            postNodeId={item.id}
-                        />}
-                />
-            );
+            itemsRender.push(<PostCard key={idx} post={item} actions={{likeable: true, shareable: true}}/>);
             itemsRender.push(<br key={idx + items.length}></br>);
         });
         return (<>{itemsRender}</>)

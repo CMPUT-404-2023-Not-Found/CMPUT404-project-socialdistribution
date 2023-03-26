3453d525290c929d262930c9989d4d81f7fd9aca
Adam Ahmed <avahmed@ualberta.ca>
2023-03-25 19:10:26 -0600

    add header dump to get-author.sh

--- bin/api/get-author.sh
+++ bin/api/get-author.sh
@@ -11,16 +11,20 @@ auth_hdr=$(./get-auth.sh 'bearer' $(cat .username) $(cat .password))
 
 get_author_url="${AUTHOR_API}/$author_uuid/"
 
-rsp=`curl -s -H "$auth_hdr" "$get_author_url"`
+hdr_dump=`mktemp`
+rsp=`curl -s -D "$hdr_dump" -H "$auth_hdr" "$get_author_url"`
 e=$?; if [ $e -ne 0 ]; then echo -n "ERR Could not GET to $get_author_url "; echo "$rsp"; exit $e; fi
 
 echo "$rsp" | grep -q "$author_uuid"
 e=$?
 if [ $e -ne 0 ]
 then
-    echo "ERR Could not get author"
+    cat "$hdr_dump" >&2
+    echo "ERR Could not get author" >&2
     echo "$rsp"
+    exit $e
 else
+    cat "$hdr_dump" >&2
     echo "$rsp" | jq 2>/dev/null
     e=$?; if [ $e -ne 0 ]; then echo "$rsp"; exit $e; fi
 fi

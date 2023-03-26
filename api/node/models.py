f398ffeaa4534639d251bd55d6845560e422d38d
Gurveer Sohal <gurveersohal433@gmail.com>
2023-03-26 17:44:59 -0600

    backend

--- api/node/models.py
+++ api/node/models.py
@@ -10,7 +10,7 @@ class Node(Model):
     host        = models.URLField(null=False, blank=False, db_index=True, max_length=128, help_text='The host that the node is served from')
     username    = models.CharField(max_length=32, db_index=True, help_text='Username used for node-to-node communication')
     password    = models.CharField(max_length=64, help_text='Password used for node-to-node communication')
-    api_prefix   = models.CharField(max_length=128, default='/api')
+    api_path   = models.CharField(max_length=128, default='/api/posts')
 
     def __str__(self):
         return f'{self.host}'

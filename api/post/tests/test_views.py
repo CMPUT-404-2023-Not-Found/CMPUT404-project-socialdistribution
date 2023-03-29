4fa52eb5823b1ba976f9c70e6dd8252696d49811
Adam Ahmed <avahmed@ualberta.ca>
2023-03-29 10:36:12 -0600

    add test to ensure like and comment count come back

--- api/post/tests/test_views.py
+++ api/post/tests/test_views.py
@@ -123,6 +123,8 @@ class PostViewTests(Base):
 
         expect_post_node_id = f'{self.author.get_node_id()}/posts/{self.author_post_uuid}'
         self.assertEqual(detail_response.data['id'], expect_post_node_id)
+        self.assertEqual(detail_response.data['likeCount'], 0)
+        self.assertEqual(detail_response.data['commentCount'], 1)
     
     def test_get_public_post_as_stranger(self):
         detail_post_url = self.get_detail_post_url(self.author.id, self.author_post_uuid)     
